facdfae690c29285e1b17d9a61038a5e64fb9c04
dugaolong <1872387570@qq.com>
2017-04-11 09:37:07 +0800

    取消提示

--- app/src/main/java/www/dugaolong/com/xianshishigongjiao/MyApplication.java
+++ app/src/main/java/www/dugaolong/com/xianshishigongjiao/MyApplication.java
@@ -31,8 +31,8 @@ public class MyApplication extends Application {
         appContext = this;
         instance = this;
 
-        AdSdk.setDebugOn();
-        AdSdk.setMockOn();
+        AdSdk.setDebugOn(); // 打开调试，输出调试信息
+        AdSdk.setMockOn();  // 调试时打开，正式发布时关闭
         //配置小米广告的sdk
         AdSdk.initialize(this, "2882303761517555679");
     }
