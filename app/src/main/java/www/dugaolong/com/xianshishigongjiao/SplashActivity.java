327c69170398f45ed843ffb29e5ce7f42e5a587e
dugaolong <1872387570@qq.com>
2017-07-04 13:56:29 +0800

    tanx init.

--- app/src/main/java/www/dugaolong/com/xianshishigongjiao/SplashActivity.java
+++ app/src/main/java/www/dugaolong/com/xianshishigongjiao/SplashActivity.java
@@ -10,7 +10,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,26 +17,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.qq.e.ads.splash.SplashAD;
-import com.qq.e.ads.splash.SplashADListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by dugaolong on 17/6/29.
  */
-public class SplashActivity extends Activity implements SplashADListener {
+public class SplashActivity extends Activity {
 
     private Context mContext;
     public boolean canJump = false;
-    private static final String SKIP_TEXT = "点击跳过 %d";
-    private SplashAD splashAD;
     private ViewGroup container;
     private TextView skipView;
     private ImageView splashHolder;
-    public static final String APPID = "1101152570";
-    public static final String SplashPosID = "8863364436303842593";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +44,7 @@ public class SplashActivity extends Activity implements SplashADListener {
         // 如果targetSDKVersion >= 23，就要申请好权限。如果您的App没有适配到Android6.0（即targetSDKVersion < 23），那么只需要在这里直接调用fetchSplashAD接口。
         if (Build.VERSION.SDK_INT >= 23) {
             checkAndRequestPermission();
-        } else {
-            // 如果是Android6.0以下的机器，默认在安装时获得了所有权限，可以直接调用SDK
-            fetchSplashAD(this, container, skipView, APPID, SplashPosID, this, 0);
         }
-
     }
 
     @Override
@@ -103,7 +91,7 @@ public class SplashActivity extends Activity implements SplashADListener {
 
         // 权限都已经有了，那么直接调用SDK
         if (lackedPermission.size() == 0) {
-            fetchSplashAD(this, container, skipView, APPID, SplashPosID, this, 0);
+            // do something
         } else {
             // 请求所缺少的权限，在onRequestPermissionsResult中再看是否获得权限，如果获得权限就可以调用SDK，否则不要调用SDK。
             String[] requestPermissions = new String[lackedPermission.size()];
@@ -125,7 +113,7 @@ public class SplashActivity extends Activity implements SplashADListener {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1024 && hasAllPermissionsGranted(grantResults)) {
-            fetchSplashAD(this, container, skipView, APPID, SplashPosID, this, 0);
+            // do something
         } else {
             // 如果用户没有授权，那么应该说明意图，引导用户去设置里面授权。
             Toast.makeText(this, "应用缺少必要的权限！请点击\"权限\"，打开所需要的权限。", Toast.LENGTH_LONG).show();
@@ -136,71 +124,6 @@ public class SplashActivity extends Activity implements SplashADListener {
         }
     }
 
-    /**
-     * 拉取开屏广告，开屏广告的构造方法有3种，详细说明请参考开发者文档。
-     *
-     * @param activity        展示广告的activity
-     * @param adContainer     展示广告的大容器
-     * @param skipContainer   自定义的跳过按钮：传入该view给SDK后，SDK会自动给它绑定点击跳过事件。SkipView的样式可以由开发者自由定制，其尺寸限制请参考activity_splash.xml或者接入文档中的说明。
-     * @param appId           应用ID
-     * @param posId           广告位ID
-     * @param adListener      广告状态监听器
-     * @param fetchDelay      拉取广告的超时时长：取值范围[3000, 5000]，设为0表示使用广点通SDK默认的超时时长。
-     */
-    private void fetchSplashAD(Activity activity, ViewGroup adContainer, View skipContainer,
-                               String appId, String posId, SplashADListener adListener, int fetchDelay) {
-        splashAD = new SplashAD(activity, adContainer, skipContainer, appId, posId, adListener, fetchDelay);
-    }
-
-    @Override
-    public void onADPresent() {
-        Log.i("AD_DEMO", "SplashADPresent");
-        splashHolder.setVisibility(View.INVISIBLE); // 广告展示后一定要把预设的开屏图片隐藏起来
-    }
-
-    @Override
-    public void onADClicked() {
-        Log.i("AD_DEMO", "SplashADClicked");
-    }
-
-    /**
-     * 倒计时回调，返回广告还将被展示的剩余时间。
-     * 通过这个接口，开发者可以自行决定是否显示倒计时提示，或者还剩几秒的时候显示倒计时
-     *
-     * @param millisUntilFinished 剩余毫秒数
-     */
-    @Override
-    public void onADTick(long millisUntilFinished) {
-        Log.i("AD_DEMO", "SplashADTick " + millisUntilFinished + "ms");
-        skipView.setText(String.format(SKIP_TEXT, Math.round(millisUntilFinished / 1000f)));
-    }
-
-    @Override
-    public void onADDismissed() {
-        Log.i("AD_DEMO", "SplashADDismissed");
-        next();
-    }
-
-    @Override
-    public void onNoAD(int errorCode) {
-        Log.i("AD_DEMO", "LoadSplashADFail, eCode=" + errorCode);
-        /** 如果加载广告失败，则直接跳转 */
-        this.startActivity(new Intent(this, MainActivity.class));
-        this.finish();
-    }
-
-    /**
-     * 设置一个变量来控制当前开屏页面是否可以跳转，当开屏广告为普链类广告时，点击会打开一个广告落地页，此时开发者还不能打开自己的App主页。当从广告落地页返回以后，
-     * 才可以跳转到开发者自己的App主页；当开屏广告是App类广告时只会下载App。
-     */
-    private void next() {
-        if (canJump) {
-            this.startActivity(new Intent(this, MainActivity.class));
-            this.finish();
-        } else {
-            canJump = true;
-        }
-    }
 
     @Override
     protected void onPause() {
@@ -208,14 +131,6 @@ public class SplashActivity extends Activity implements SplashADListener {
         canJump = false;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (canJump) {
-            next();
-        }
-        canJump = true;
-    }
 
     /** 开屏页一定要禁止用户对返回按钮的控制，否则将可能导致用户手动退出了App而广告无法正常曝光和计费 */
     @Override
